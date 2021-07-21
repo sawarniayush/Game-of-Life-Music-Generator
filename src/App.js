df6d0f90a5e2bed7658f2f09e6f2d39c6c690019
Ayush Sawarni <sav@Ayushs-MacBook-Air.local>
2021-07-21 21:29:02 +0530

    added basic nav bar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import GameOfLife from './components/gameOfLiveCanvas'
 import Blog from './components/gameOfLife'
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, HashRouter } from 'react-router-dom'
 function App() {
   return (
-    <Router>
+    <HashRouter>
         <Route path="/" component={NavBar}/>
         <Route path="/:id" component={NavBar}/>
         <Route exact path="/blog" component={Blog}/>
         <Route exact path="/" component={GameOfLife}/>
         <Route exact path="/home" component={GameOfLife}/>
-    </Router>
+    </HashRouter>
   );
 }
 
